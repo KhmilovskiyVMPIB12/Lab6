294928f0eec0bbf93164339845c1709c758a3d69
KhmilovskiyVMPIB12 <125645185+KhmilovskiyVMPIB12@users.noreply.github.com>
2023-06-09 16:55:51 +0700

    Add files via upload

---
+++ 6/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea17 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend17 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series17 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea18 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend18 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series18 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea17.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea17);
+            legend17.Name = "Legend1";
+            this.chart1.Legends.Add(legend17);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series17.ChartArea = "ChartArea1";
+            series17.Legend = "Legend1";
+            series17.Name = "Series1";
+            this.chart1.Series.Add(series17);
+            this.chart1.Size = new System.Drawing.Size(432, 330);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea18.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea18);
+            legend18.Name = "Legend1";
+            this.chart2.Legends.Add(legend18);
+            this.chart2.Location = new System.Drawing.Point(497, 12);
+            this.chart2.Name = "chart2";
+            series18.ChartArea = "ChartArea1";
+            series18.Legend = "Legend1";
+            series18.Name = "Series1";
+            this.chart2.Series.Add(series18);
+            this.chart2.Size = new System.Drawing.Size(480, 330);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 393);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(643, 393);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(734, 393);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1192, 655);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 6/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System.Windows.Forms;
+using System;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            FunctionDelegate equation1 = x => 2 * x * x + 3 * x + 4;
+            Series series1 = new Series();
+            series1.ChartType = SeriesChartType.Line;
+            DrawFunc(-10.0, 10.0, series1, equation1);
+            chart1.Series.Add(series1);
+
+            // Создание экземпляра делегата для уравнения SinEquation
+            FunctionDelegate equation2 = x => Math.Sin(x) / x;
+            Series series2 = new Series();
+            series2.ChartType = SeriesChartType.Line;
+            DrawFunc(-10.0, 10.0, series2, equation2);
+            chart2.Series.Add(series2);
+
+            // Создание экземпляра интегратора RectangleIntegrator
+            Integrator integrator1 = new RectangleIntegrator();
+            integrator1.SetStep(0.1);
+            double result1 = integrator1.Integrate(equation1, -10.0, 10.0);
+            string methodName1 = integrator1.GetName();
+
+            // Создание экземпляра интегратора TrapezoidIntegrator
+            Integrator integrator2 = new TrapezoidIntegrator();
+            integrator2.SetStep(0.1);
+            double result2 = integrator2.Integrate(equation1, -10.0, 10.0);
+            string methodName2 = integrator2.GetName();
+
+            label1.Text = methodName1;
+            label2.Text = result1.ToString();
+
+            label3.Text = methodName2;
+            label4.Text = result2.ToString();
+
+            // Отображение результатов интегрирования
+            MessageBox.Show($"Интеграл ({methodName1}) = {result1}");
+            MessageBox.Show($"Интеграл ({methodName2}) = {result2}");
+        }
+        public void DrawFunc(double x1, double x2, Series series, FunctionDelegate function)
+        {
+            Chart chart = new Chart();
+
+            chart.Series.Clear();
+
+            double step = 0.1;
+
+            for (double x = x1; x <= x2; x += step)
+            {
+                double y = function(x);
+                series.Points.AddXY(x, y);
+            }
+        }
+    }
+
+    public abstract class Equation //Базовый Класс
+    {
+        public abstract double Evaluate(double x); //Тут мы обьявляем функцию(абстрактную)
+    }
+
+    public class QuadraticEquation : Equation //Наследуем базовый класс
+    {
+        private double a, b, c;
+
+        public QuadraticEquation(double a, double b, double c) //Это конструктор в котором мы обьявляем наши переменные
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public override double Evaluate(double x) //Тут переопределяем функцию и задаем ей действия
+        {
+            return a * x * x + b * x + c;
+        }
+    }
+
+    public class SinEquation : Equation //Тут тоже самое только отличется  вычислениями
+    {
+        private double a;
+
+        public SinEquation(double a)
+        {
+            this.a = a;
+        }
+
+        public override double Evaluate(double x)
+        {
+            if (x != 0)
+                return Math.Sin(a * x) / x;
+            else
+                return 1; // Чтобы избежать деления на ноль
+        }
+    }
+
+    public delegate double FunctionDelegate(double x);
+
+    public abstract class Integrator //Это уже другой базовый класс
+    {
+        protected double h;
+
+        public abstract double Integrate(FunctionDelegate function, double x1, double x2); //Абстрактная функция, в целом тоже самое что и класс, только функция
+
+        public abstract string GetName();
+
+        public void SetStep(double step)
+        {
+            h = step;
+        }
+    }
+
+    public class RectangleIntegrator : Integrator
+    {
+        public override double Integrate(FunctionDelegate function, double x1, double x2)
+        {
+            double sum = 0.0;
+
+            for (double x = x1; x < x2; x += h)
+            {
+                double y = function(x);
+                sum += y * h;
+            }
+
+            return sum;
+        }
+
+        public override string GetName()
+        {
+            return "Метод прямоугольников";
+        }
+    }
+
+    public class TrapezoidIntegrator : Integrator
+    {
+        public override double Integrate(FunctionDelegate function, double x1, double x2)
+        {
+            double sum = 0.0;
+
+            for (double x = x1; x < x2; x += h)
+            {
+                double y1 = function(x);
+                double y2 = function(x + h);
+                sum += (y1 + y2) * h / 2;
+            }
+
+            return sum;
+        }
+
+        public override string GetName()
+        {
+            return "Метод трапеций";
+        }
+    }
+}
